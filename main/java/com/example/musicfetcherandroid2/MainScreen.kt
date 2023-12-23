4dbb8174866603fa49949f44a884ac0407fb7844
Sebastian Balderrama <sebastian.balderr@gmail.com>
2023-12-22 22:43:29 -0500

    Update MainScreen.kt
    Added a nice fade in animation for the MainScreen Greeting

--- main/java/com/example/musicfetcherandroid2/MainScreen.kt
+++ main/java/com/example/musicfetcherandroid2/MainScreen.kt
@@ -1,22 +1,30 @@
 // MainScreen.kt
 package com.example.musicfetcherandroid2
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.*
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.border
-import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.compose.ui.text.style.TextAlign
+import com.example.musicfetcherandroid2.ui.theme.Purple80
 
 @Composable
 fun MainScreen(navController: NavHostController) {
+    var greetingVisible by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        greetingVisible = true
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -24,9 +32,24 @@ fun MainScreen(navController: NavHostController) {
         verticalArrangement = Arrangement.Top, // Set verticalArrangement to Top
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Greeting("Welcome to MusicFetcher")
-        Spacer(modifier = Modifier.weight(1f)) // Spacer with weight to push the next item to the center
-        // Button directly inside the main Column
+        AnimatedVisibility(
+            visible = greetingVisible,
+            enter = fadeIn(
+                animationSpec = tween(durationMillis = 1000)
+            )
+        ) {
+            Greeting("Welcome to MusicFetcher")
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center, // Set verticalArrangement to Top
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        //Spacer(modifier = Modifier.weight(1f))
         Button(onClick = { navController.navigate("second_screen") }) {
             Text("Go to Second Screen")
         }
@@ -37,7 +60,9 @@ fun MainScreen(navController: NavHostController) {
 fun Greeting(s: String) {
     Text(
         text = s,
-        style = MaterialTheme.typography.displayLarge, // Adjust the style as needed
+        color = Purple80,
+        style = MaterialTheme.typography.displayMedium, // Adjust the style as needed
+        textAlign = TextAlign.Center,
         modifier = Modifier
             .border(1.dp,Color.Black)
             .padding(8.dp) // Add some padding for better visibility
